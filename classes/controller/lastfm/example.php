8eb441b7e6b15361905822be7a65047419ccd43b
Jeremy Bush <contractfrombelow@gmail.com>
2010-09-28 15:03:25 -0500

    adding example controller

---
+++ classes/controller/lastfm/example.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @package    Last.fm
+ * @author     Jeremy Bush
+ * @copyright  (c) 2010 Jeremy Bush
+ */
+class Controller_LastFM_Example extends Controller
+{
+	/**
+	 * 
+	 *
+	 * @return null
+	 */
+	public function action_index()
+	{
+		$lastfm = new LastFM;
+
+		LastFM::$session = Session::instance()->get('lastfm_session');
+
+		if ( ! $lastfm->has_valid_session())
+			LastFM::authorize('http://lastfm/index.php/lastfm/example/process');
+		var_dump(Session::instance()->get('lastfm_session'));
+		var_dump($lastfm->api('user.getLovedTracks', array(
+			'user' => 'zombor_666',
+		)));
+	}
+
+	/**
+	 * Does stuff
+	 *
+	 * @return null
+	 */
+	public function action_process()
+	{
+		$token = arr::get($_GET, 'token');
+		$lastfm = new LastFM;
+		$foo = $lastfm->fetch_service_session($token);
+		Session::instance()->set('lastfm_session', $foo);
+		$this->request->redirect('lastfm/example/index');
+	}
+}
